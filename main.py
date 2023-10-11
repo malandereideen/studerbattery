cb29454f098d142c58442c92f331ee2c86730b92
malandereideen <73538845+malandereideen@users.noreply.github.com>
2023-10-11 16:13:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import wifimgr
+from machine import Pin
+from time import sleep
+from netzwerk import *
+from geheim import *
+import display
+import time
+
+try:
+    import usocket as socket
+except:
+    import socket
+    
+timeout = 900
+
+wlan = wifimgr.get_connection()
+
+if wlan is None:
+    display.clean()
+    display.no_connection()
+    while True:
+        pass
+            
+display.clean()
+sleep(1)
+    
+while True:
+    mytime = wifi.get_zeit()
+    mydatum = wifi.date_text(mytime)
+    myuhrzeit = wifi.time_text(mytime)
+    mybattery = wifi.get_battery(std_uid,std_mail,std_pass)
+    display.clean()
+    display.zeit(mydatum,myuhrzeit)
+    display.scala()
+    display.ladung(mybattery)
+    sleep(timeout)
+        
